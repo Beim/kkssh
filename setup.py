78a3086afc72b20d4f2496311ae975df923469bc
Beim <804908499@qq.com>
2023-04-04 00:29:12 +1200

    update repo

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='kkssh',
-    version='0.8',
+    version='0.9',
     description='ssh client',
     author='xingkun',
     author_email='xingkunliu@qq.com',
-    url='https://github.com/Beim/kssh',
+    url='https://github.com/Beim/kkssh',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     keywords='ssh client',
