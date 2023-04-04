529fc413a47c5582bfd0499bc80df5964c50fcce
Beim <804908499@qq.com>
2023-04-04 19:44:03 +1200

    use fuzzy search

--- src/kkssh/main.py
+++ src/kkssh/main.py
@@ -1,56 +1,38 @@
 import os
 import argparse
 import paramiko
-
-
-def edit_distance(s1, s2):
-    m, n = len(s1), len(s2)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    
-    for i in range(1, m + 1):
-        dp[i][0] = i
-    
-    for j in range(1, n + 1):
-        dp[0][j] = j
-    
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = dp[i-1][j-1]
-            else:
-                dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
-    
-    return dp[m][n]
+from fuzzywuzzy import process
 
 def connect(host: str):
-    os.system('ssh %s' % host)
+    os.system(f'ssh {host}')
 
-def format_hostname(cfg: dict):
-    if cfg is None:
-        cfg = {}
-    hostname = cfg.get('hostname', ' ')
-    port = cfg.get('port', '22')
-    username = cfg.get('user', ' ')
-    if port != '22':
-        return f'{username}@{hostname}:{port}'
-    return f'{username}@{hostname}'
-
-def connect_to_user_selected(hosts: list[str], ssh_config: paramiko.SSHConfig):
+def connect_to_user_selected(hosts: list[str]):
     # Let user select
-    print('#\tHost\tHostName')
+    ss = '#\tHost\n'
     for idx in range(len(hosts)):
         host = hosts[idx]
-        cfg = ssh_config.lookup(host)
-        formated_hostname = format_hostname(cfg)
-        print(f'{idx}\t{host}\t{formated_hostname}')
-    print('Select Index: [0]', end =" ")
-    selected_idx = input()
-    if not selected_idx:
-        selected_idx = 0
+        ss += f'{idx}\t{host}\n'
+    ss += 'Select Index or Host: [0]'
+    print(ss, end=" ", flush=True)
+
     try:
-        selected_idx = int(selected_idx)
-    except Exception:
-        print('Index must be an integer')
+        selected = input()
+    except KeyboardInterrupt:
+        print('\nUser cancelled')
+        return
+    # default to 0
+    if not selected:
+        selected = '0'
+    # user input host
+    if not selected.isdigit():
+        connect(selected)
+        return
+
+    # user input index
+    try:
+        selected_idx = int(selected)
+    except ValueError:
+        print('Invalid input')
         return
     if selected_idx >= len(hosts):
         print(f'Index must be less than {len(hosts)}')
@@ -59,8 +41,7 @@ def connect_to_user_selected(hosts: list[str], ssh_config: paramiko.SSHConfig):
     host = hosts[selected_idx]
     connect(host)
 
-def main():
-    # Parse arguments
+def parse_sys_args():
     parser = argparse.ArgumentParser(
                         prog='kkssh',
                         description='ssh wrapper')
@@ -69,15 +50,17 @@ def main():
     parser.add_argument('--editor', default='code')
     parser.add_argument('-e', '--edit', action='store_true')
     parser.add_argument('hostname', nargs='?')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    # Parse arguments
+    args = parse_sys_args()
     ssh_config_path = args.config
     target_host = args.hostname
-    editor = args.editor
-    edit = args.edit
 
-    if edit:
+    if args.edit:
         print(f'Opening {ssh_config_path}')
-        os.system(f'{editor} {ssh_config_path}')
+        os.system(f'{args.editor} {ssh_config_path}')
         return
 
     # Load ssh config
@@ -88,29 +71,21 @@ def main():
 
     # No hostname provided, let user select
     if not target_host:
-        connect_to_user_selected(hosts, ssh_config)
+        connect_to_user_selected(hosts)
         return
 
     # Direct connect if hostname match
     if target_host in hosts:
-        cfg = ssh_config.lookup(target_host)
-        print(f'connecting to {format_hostname(cfg)}')
         connect(target_host)
         return
 
-    # Find alternatives
-    hosts_with_distance = []
-    for host in hosts:
-        distance = edit_distance(target_host, host)
-        if distance < max(len(host), len(target_host)):
-            hosts_with_distance.append([host, distance])
-    hosts_with_distance = sorted(hosts_with_distance, key=lambda x: x[1])
-    if not hosts_with_distance:
+    # Fuzzy search
+    matches = process.extract(target_host, hosts)
+    if not matches:
         print('Not found')
         return
-    hosts = [i[0] for i in hosts_with_distance]
-    connect_to_user_selected(hosts, ssh_config)
-    
+    match_hosts = [i[0] for i in matches]
+    connect_to_user_selected(match_hosts)
 
 if __name__ == '__main__':
     main()
